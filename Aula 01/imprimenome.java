fe67745bce60fb8bd8b77a815cd71ffd345a1cb8
Gabriel(Genos) <gabrielmartinspastoriza3266@gmail.com>
2023-09-11 17:26:34 -0300

    Primeiro exercício da aula 01- ImprimeNome

---
+++ Aula 01/imprimenome.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ImprimeNome {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+          System.out.print("Digite seu nome: ");
+           String nome = scanner.nextLine();
+              scanner.close();
+                  System.out.println("Muito prazer: " + nome);
+    }
+}
+
