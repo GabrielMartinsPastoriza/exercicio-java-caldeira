c29a2638dd95ac60a568cf038203db73db68c90b
Gabriel(Genos) <gabrielmartinspastoriza3266@gmail.com>
2023-09-14 12:05:30 -0300

    Resolvendo exercícios Aula 01

--- Aula 01/labs01/src/com/github/javacaldeira/ImprimeNome.java
+++ Aula 01/labs01/src/com/github/javacaldeira/ImprimeNome.java
@@ -2,7 +2,7 @@ package com.github.javacaldeira;
 
 import java.util.Scanner;
 
-public class imprimeNome {
+public class ImprimeNome {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
